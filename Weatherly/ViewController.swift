c5670ce33cbb8790ae0b6af0461e53f5577e0c1f
Ivan Kulundžić <ivan.kulundzic.mail@gmail.com>
2020-04-02 21:22:16 +0200

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Ivan Kulundzic on 02/04/2020.
+//  Copyright © 2020 Ivan Kulundzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
